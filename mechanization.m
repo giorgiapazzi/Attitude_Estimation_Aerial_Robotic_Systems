21d3f492ea728cad28e8a852a7cda661798a2b4c
giorgiapazzi <giorgia.pazzi99@gmail.com>
2023-06-21 16:21:30 +0200

    attitude estimation with gyro noise and bias

--- mechanization.m
+++ mechanization.m
@@ -62,7 +62,7 @@ true_attitude_angles(1,:)= [roll_0,pitch_0,yaw_0];
 
 for i = 1 : (numSamples)
     % Compute roll angle
-    roll = wrapToPi(atan2(-R(3,2,i),-R(3,3,i)));
+    roll = wrapTo2Pi(atan2(-R(3,2,i),-R(3,3,i)));
     roll = round(roll,2);
     % Rotation of -pi or +pi is equivalent: control to prevent oscillations
     % between -pi and +pi
@@ -78,7 +78,7 @@ for i = 1 : (numSamples)
     true_attitude_angles(i+1,1) = roll;
 
     % Compute pitch angle
-    pitch = wrapToPi(atan2(-R(3,1,i),-sqrt((R(3,2,i))^2 + (R(3,3,i))^2)));
+    pitch = wrapTo2Pi(atan2(-R(3,1,i),-sqrt((R(3,2,i))^2 + (R(3,3,i))^2)));
     pitch = round(pitch,2);
     % Rotation of -pi or +pi is equivalent: control to prevent oscillations
     % between -pi and +pi
@@ -94,7 +94,7 @@ for i = 1 : (numSamples)
     true_attitude_angles(i+1,2) = pitch;
 
     % Compute yaw angle
-    yaw = wrapToPi(atan2(-R(2,1,i),-R(1,1,i)));
+    yaw = wrapTo2Pi(atan2(-R(2,1,i),-R(1,1,i)));
     yaw = round(yaw,2);
     % Rotation of -pi or +pi is equivalent: control to prevent oscillations
     % between -pi and +pi
@@ -111,7 +111,7 @@ for i = 1 : (numSamples)
 end
 
 %% Plot
-t = (0:(numSamples))/fs;  % istanti di tempo in cui vengono fornite le misure
+t = (0:(numSamples))/fs;  % time when measurements are provided
 
 figure(1)
 plot(t,true_attitude_angles(:,1)')
@@ -125,7 +125,7 @@ xlabel('t [s]')
 ylabel('Roll-pitch-yaw angles [rad]')
 
 
-%% Euler angles kinematics
+%% Euler angles kinematics wihout noise
 % roll_dot = omega_x + sin(roll)*tan(pitch)*omega_y + cos(roll)*tan(pitch)*omega_z
 % pitch_dot = cos(roll)*omega_y - sin(roll)*omega_z
 % yaw_dot = sin(roll)/cos(pitch) * omega_y + cos(roll)/cos(pitch) * omega_z
@@ -141,22 +141,22 @@ for i = 2 : (numSamples+1)
 
     roll_dot = angvel_x(i-1) + sin(roll)*tan(pitch)*angvel_y(i-1) + cos(roll)*tan(pitch)*angvel_z(i-1);
     attitude_angles(i,1) = attitude_angles(i-1,1) + roll_dot*dt;
-    attitude_angles(i,1) = wrapToPi(attitude_angles(i,1));
+    attitude_angles(i,1) = wrapTo2Pi(attitude_angles(i,1));
 
     pitch_dot = cos(roll)*angvel_y(i-1) - sin(roll)*angvel_z(i-1);
     attitude_angles(i,2) = attitude_angles(i-1,2) + pitch_dot*dt;
-    attitude_angles(i,2) = wrapToPi(attitude_angles(i,2));
+    attitude_angles(i,2) = wrapTo2Pi(attitude_angles(i,2));
 
     yaw_dot = sin(roll)/cos(pitch) * angvel_y(i-1) + cos(roll)/cos(pitch) * angvel_z(i-1);
     attitude_angles(i,3) = attitude_angles(i-1,3) + yaw_dot*dt;
-    attitude_angles(i,3) = wrapToPi(attitude_angles(i,3));
+    attitude_angles(i,3) = wrapTo2Pi(attitude_angles(i,3));
 end
 
 
 %% Plot
-t = (0:(numSamples))/fs;  % istanti di tempo in cui vengono fornite le misure
+t = (0:(numSamples))/fs;  % time when measurements are provided
 
-figure(1)
+figure(2)
 plot(t,attitude_angles(:,1)')
 hold on
 plot(t,attitude_angles(:,2)')
@@ -166,3 +166,65 @@ legend('Roll','Pitch','Yaw')
 title('Attitude estimation')
 xlabel('t [s]')
 ylabel('Roll-pitch-yaw angles [rad]')
+
+%% Euler angles kinematics with noise
+% roll_dot = omega_x + sin(roll)*tan(pitch)*omega_y + cos(roll)*tan(pitch)*omega_z
+% pitch_dot = cos(roll)*omega_y - sin(roll)*omega_z
+% yaw_dot = sin(roll)/cos(pitch) * omega_y + cos(roll)/cos(pitch) * omega_z
+
+% Discretization with forward Euler method
+attitude_angles_N = zeros(numSamples+1,3);
+attitude_angles_N(1,:)= [roll_0,pitch_0,yaw_0];   % we assume to know exactly the initial position of the drone
+
+for i = 2 : (numSamples+1)
+    roll = attitude_angles_N(i-1,1);
+    pitch = attitude_angles_N(i-1,2);
+    yaw = attitude_angles_N(i-1,3);
+
+    roll_dot = angvel_x_N(i-1) + sin(roll)*tan(pitch)*angvel_y_N(i-1) + cos(roll)*tan(pitch)*angvel_z_N(i-1);
+    attitude_angles_N(i,1) = attitude_angles_N(i-1,1) + roll_dot*dt;
+    attitude_angles_N(i,1) = wrapTo2Pi(attitude_angles_N(i,1));
+
+    pitch_dot = cos(roll)*angvel_y_N(i-1) - sin(roll)*angvel_z_N(i-1);
+    attitude_angles_N(i,2) = attitude_angles_N(i-1,2) + pitch_dot*dt;
+    attitude_angles_N(i,2) = wrapTo2Pi(attitude_angles_N(i,2));
+
+    yaw_dot = sin(roll)/cos(pitch) * angvel_y_N(i-1) + cos(roll)/cos(pitch) * angvel_z_N(i-1);
+    attitude_angles_N(i,3) = attitude_angles_N(i-1,3) + yaw_dot*dt;
+    attitude_angles_N(i,3) = wrapTo2Pi(attitude_angles_N(i,3));
+end
+
+%% Plot
+t = (0:(numSamples))/fs;  % time when measurements are provided
+
+figure(2)
+plot(t,attitude_angles_N(:,1)')
+hold on
+plot(t,attitude_angles_N(:,2)')
+hold on
+plot(t,attitude_angles_N(:,3)')
+legend('Roll','Pitch','Yaw')
+title('Attitude estimation')
+xlabel('t [s]')
+ylabel('Roll-pitch-yaw angles [rad]')
+
+
+%% Error plot
+for i=1 : (numSamples+1)
+    error_roll(i) = true_attitude_angles(i,1) - attitude_angles_N(i,1);
+    error_pitch(i) = true_attitude_angles(i,2) - attitude_angles_N(i,2);
+    error_yaw(i) = true_attitude_angles(i,3) - attitude_angles_N(i,3);
+end
+
+t = (0:(numSamples))/fs;  % time when measurements are provided
+
+figure(3)
+plot(t,error_roll)
+hold on
+plot(t,error_pitch)
+hold on
+plot(t,error_yaw)
+legend('Roll Error','Pitch Error','Yaw Error')
+title('Attitude estimation error')
+xlabel('t [s]')
+ylabel('Roll-pitch-yaw angles error [rad]')
